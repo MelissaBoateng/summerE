69c4a4d3f93fa8f4d7cc61a14940dd500205d720
MelissaBoateng <67985151+MelissaBoateng@users.noreply.github.com>
2021-06-29 15:40:39 +0300

    Add files via upload

---
+++ Quiz3/app.js
@@ -0,0 +1,35 @@
+function payWithPaystack() {
+
+    var handler = PaystackPop.setup({ 
+        key: 'pk_test_7f942e18ff5ccfad7fb8ab295ac0e4a49308f768', //put your public key here
+        email: 'student@gmail.com', //put your customer's email here
+        amount: 900, //amount the customer is supposed to pay
+        metadata: {
+            custom_fields: [
+                {
+                    display_name: "Mobile Number",
+                    variable_name: "mobile_number",
+                    value: "+233 2412345678" //customer's mobile number
+                }
+            ]
+        },
+        callback: function (response) {
+            //after the transaction have been completed
+            //make post call  to the server with to verify payment 
+            //using transaction reference as post data
+            $.post("verify.php", {reference:response.reference}, function(status){
+                if(status == "success")
+                    //successful transaction
+                    alert('Transaction was successful');
+                else
+                    //transaction failed
+                    alert(response);
+            });
+        },
+        onClose: function () {
+            //when the user close the payment modal
+            alert('Transaction cancelled');
+        }
+    });
+    handler.openIframe(); //open the paystack's payment modal
+}
